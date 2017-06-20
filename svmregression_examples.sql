448a338935740ab9b1e735de42787c0dcf0819b0
soniyashah <soniya.shah@hpe.com>
2017-06-20 08:32:06 -0400

    SVM for regression examples

---
+++ svmregression_examples.sql
@@ -0,0 +1,9 @@
+--SVM Regression Functions
+
+--SVM_REGRESSOR: 
+
+SELECT SVM_REGRESSOR('mySvmRegModel', 'faithful', 'eruptions', 'waiting' USING PARAMETERS error_tolerance=0.1, max_iterations=100);
+
+--PREDICT_SVM_REGRESSOR: 
+
+SELECT PREDICT_SVM_REGRESSOR(waiting USING PARAMETERS model_name='mySvmRegModel') FROM faithful ORDER BY id;
