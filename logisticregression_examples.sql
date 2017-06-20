3cbce80cd81bdbad3de9911829b10504692380b5
soniyashah <soniya.shah@hpe.com>
2017-06-20 08:23:19 -0400

    Logistic regression examples

---
+++ logisticregression_examples.sql
@@ -0,0 +1,10 @@
+--Logistic Regression Functions 
+
+--LOGISTIC_REG: 
+
+SELECT LOGISTIC_REG('myLogisticRegModel', 'mtcars', 'am','mpg, cyl, disp, hp, drat, wt, qsec, vs, gear, carb'
+                        USING PARAMETERS exclude_columns='hp', optimizer='BFGS');
+
+--PREDICT_LOGISTIC_REG:
+
+SELECT car_model,PREDICT_LOGISTIC_REG(mpg, cyl, disp, drat, wt, qsec, vs, gear, carb USING PARAMETERS model_name='myLogisticRegModel')FROM mtcars;
