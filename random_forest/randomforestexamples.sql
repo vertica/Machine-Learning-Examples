973f86e126e4a08c26aa0d3a3ee6c91a93f1d7ee
soniyashah <soniya.shah@hpe.com>
2017-12-21 10:58:02 -0500

    Update randomforestexamples.sql

--- random_forest/randomforestexamples.sql
+++ random_forest/randomforestexamples.sql
@@ -5,10 +5,19 @@
 SELECT RF_CLASSIFIER ('myRFModel', 'iris', 'Species', 'Sepal_Length, Sepal_Width, Petal_Length, Petal_Width' 
 USING PARAMETERS ntree=100, sampling_size=0.3);
 
+--RF_REGRESSOR: 
+ SELECT RF_REGRESSOR ('myRFRegressorModel', 'mtcars', 'carb', 'mpg, cyl, hp, drat, wt' USING PARAMETERS
+ntree=100, sampling_size=0.3);
+
 --PREDICT_RF_CLASSIFIER: 
 
 SELECT PREDICT_RF_CLASSIFIER (Sepal_Length, Sepal_Width, Petal_Length, Petal_Width
                                   USING PARAMETERS model_name='myRFModel') FROM iris;
+
+--PREDICT_RF_REGRESSOR: 
+
+SELECT PREDICT_RF_REGRESSOR (mpg,cyl,hp,drat,wt 
+USING PARAMETERS model_name='myRFRegressorModel')FROM mtcars;
 
 --PREDICT_RF_CLASSIFIER_CLASSES: 
 SELECT PREDICT_RF_CLASSIFIER_CLASSES(Sepal_Length, Sepal_Width, Petal_Length, Petal_Width
